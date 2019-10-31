49c8168d95dfb2d2678fe8a29ea87a378f74dc50
Rod Elias <rodiney@gmail.com>
2019-10-30 21:18:02 -0300

    add missing type hint

--- src/Exception/CorruptInputException.php
+++ src/Exception/CorruptInputException.php
@@ -7,7 +7,7 @@ use Symfony\Component\Process\Exception\RuntimeException;
 
 final class CorruptInputException extends \InvalidArgumentException implements BrotliException
 {
-    public static function create(RuntimeException $previous)
+    public static function create(RuntimeException $previous): self
     {
         return new self('Input data is not valid Brotli.', 0, $previous);
     }
